5b9e3003c03cf210459454ed95de9b495541cb0a
gingercat98 <averykle009@gmail.com>
2021-03-08 20:43:51 -0800

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World App!'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 400.0,
+            decoration: BoxDecoration(
+              color: Colors.pink,
+            ),
+            child: Center(
+              child: Text(
+                'Hello world',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
